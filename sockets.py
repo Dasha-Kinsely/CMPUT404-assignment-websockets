b78b2d6bbd4cf8f554e8fdfb231ead3cb4bb7686
Dasha-Kinsely <dashakinsely@gmail.com>
2020-03-23 16:43:35 -0600

    unfinished

--- sockets.py
+++ sockets.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
+#
 # Copyright (c) 2013-2014 Abram Hindle
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# Copyright (c) 2020 Yun Tai Liu
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -14,7 +20,7 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, url_for, redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
@@ -25,6 +31,29 @@ import os
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+# The following class is based on lecture-slide code: https://github.com/uofa-cmput404/cmput404-slides/blob/master/examples/WebSocketsExamples/chat.py
+clients = list()
+
+class Utility:
+    def send_all(self, obj):
+        for client in clients:
+            clients.put(obj)
+    
+    def send_json(self, obj):
+        self.send_all(json.dumps(obj))
+
+    def dump_json(self, obj):
+        return json.dumps(obj), 200
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
 
 class World:
     def __init__(self):
@@ -63,25 +92,49 @@ myWorld = World()
 
 def set_listener( entity, data ):
     ''' do something with the update ! '''
+    Utility.send_json({entity:data})
 
 myWorld.add_set_listener( set_listener )
         
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    fname = "/static/index.html"
+    return redirect(fname)
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    try:
+        while True:
+            obj = ws.receive()
+            if (obj != None):
+                packet = json.loads(obj)
+                Utility.send_json(packet)
+            else:
+                break
+    except:
+        '''Not Defined'''
+        #return None
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn(read_ws, ws, client)
+    try: 
+        while True:
+            # block here
+            msg = client.get()
+            ws.send(msg)
+    except Exception as e:# WebSocketError as e:
+        print("WS Error %s" % e)
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 # I give this to you, this is how you get the raw body/data portion of a post in flask
@@ -99,25 +152,32 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    data = flask_post_json()
+    for i in data:
+        try:
+            myWorld.update(entity, i, data[i])
+        except:
+            myWorld.set(entity, data)
+    res = myWorld.get(entity)
+    Utility.dump_json(res)
+
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    Utility.dump_json(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    Utility.dump_json(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
-
-
+    myWorld.clear()
+    Utility.dump_json(myWorld.world())
 
 if __name__ == "__main__":
     ''' This doesn't work well anymore:
